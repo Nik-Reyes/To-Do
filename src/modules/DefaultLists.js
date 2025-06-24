4841ba5b33b0d18dd4c7e68da71f3aba431e117c
“Nik <nikolaspreyes@gmail.com>
2025-06-23 18:52:19 -0700

    refactor: add default lists and tasks

---
+++ src/modules/DefaultLists.js
@@ -0,0 +1,40 @@
+import List from "./List.js";
+import { defaultItems } from "./DefaultToDoItems.js";
+
+export default function CreateDefaultLists() {
+  const lists = {};
+  const myListStage = [];
+  const systemListStage = [];
+  const myLists = ["Groceries"];
+  const systemLists = ["Today", "Scheduled", "All Tasks", "Completed"];
+
+  const createLists = () => {
+    systemLists.forEach((title) => {
+      systemListStage.push(new List(title));
+    });
+
+    myLists.forEach((title) => {
+      myListStage.push(new List(title));
+    });
+  };
+
+  const addMyListItems = () => {
+    defaultItems.forEach((item) => {
+      myListStage[0].addToDo(item);
+    });
+  };
+
+  const stageLists = () => {
+    createLists();
+    addMyListItems();
+  };
+
+  const pushLists = () => {
+    stageLists();
+    lists.systemLists = systemListStage;
+    lists.myLists = myListStage;
+    return lists;
+  };
+
+  return pushLists();
+}
