58565fca96bd32572eb8d749e9e6edaa2a094e90
“Nik <nikolaspreyes@gmail.com>
2025-06-27 15:06:27 -0700

    refactor: use attributes property to assign all attributes of an element

--- src/utils/createElement.js
+++ src/utils/createElement.js
@@ -1,12 +1,21 @@
 export const utils = {
-  generateElement: (type, classes, content) => {
+  generateElement: (type, attributes = {}, content = null) => {
     const element = document.createElement(type);
-    element.classList.add(...classes);
-    if (content && content.includes(".jpg")) {
-      element.src = content;
-    }
-    if (content && !content.includes(".jpg")) {
+    if (content) {
       element.innerText = content;
+    }
+
+    if (attributes && attributes instanceof Object) {
+      Object.entries(attributes)
+        .filter(([key, value]) => {
+          return value.toString() !== "false" && value !== undefined;
+        })
+        .forEach(([key, value]) => {
+          element.setAttribute(
+            key,
+            value === true || value === undefined ? key : value
+          );
+        });
     }
     return element;
   },
