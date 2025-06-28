49b4a76462d3e85970cbae4908b2fc4d5381c671
“Nik <nikolaspreyes@gmail.com>
2025-06-27 19:52:04 -0700

    build(import): add popover polyfill to main.js

--- src/main.js
+++ src/main.js
@@ -1,68 +1,74 @@
 import Manager from "./modules/manager.js";
+import renderUi from "./modules/RenderUI.js";
+import "@oddbird/popover-polyfill";
+import "@oddbird/css-anchor-positioning";
+
 const manager = new Manager();
+import "./style.css";
+renderUi();
 
-function switchList() {
-  const listNames = manager.lists
-    .map((list, i) => `${i + 1}. ${list.title}`)
-    .join("\n");
+// function switchList() {
+//   const listNames = manager.lists
+//     .map((list, i) => `${i + 1}. ${list.title}`)
+//     .join("\n");
 
-  const choice = parseInt(
-    prompt(
-      `Which list would you like to switch to?\n\nEnter the list number:\n${listNames}`
-    )
-  );
-  manager.currentList = choice - 1;
-}
+//   const choice = parseInt(
+//     prompt(
+//       `Which list would you like to switch to?\n\nEnter the list number:\n${listNames}`
+//     )
+//   );
+//   manager.currentList = choice - 1;
+// }
 
-function addTask() {
-  manager.addTask(prompt("Enter task title"));
-}
+// function addTask() {
+//   manager.addTask(prompt("Enter task title"));
+// }
 
-function newList() {
-  manager.addList(prompt("Enter the title of the List"));
-}
+// function newList() {
+//   manager.addList(prompt("Enter the title of the List"));
+// }
 
-function editTask() {}
+// function editTask() {}
 
-function viewListTasks() {
-  alert(manager.formattedTasks);
-}
+// function viewListTasks() {
+//   alert(manager.formattedTasks);
+// }
 
-function deleteTask() {
-  const taskToDelete = parseInt(
-    prompt(
-      `Enter the index of the task to be deleted (starting from 1)\n${manager.formattedTasks} `
-    )
-  );
-  manager.deleteTask(taskToDelete);
-}
+// function deleteTask() {
+//   const taskToDelete = parseInt(
+//     prompt(
+//       `Enter the index of the task to be deleted (starting from 1)\n${manager.formattedTasks} `
+//     )
+//   );
+//   manager.deleteTask(taskToDelete);
+// }
 
-let choice = null;
-while (true) {
-  choice = parseInt(
-    prompt(
-      `what would you like to do?\n1. Select another list\n2. Add task to current list (${manager.currentListTitle})\n3. Create a new List\n4. Edit a task of the current list (${manager.currentListTitle})\n5. View all tasks of current List\n6. Delete a task`
-    )
-  );
+// let choice = null;
+// while (true) {
+//   choice = parseInt(
+//     prompt(
+//       `what would you like to do?\n1. Select another list\n2. Add task to current list (${manager.currentListTitle})\n3. Create a new List\n4. Edit a task of the current list (${manager.currentListTitle})\n5. View all tasks of current List\n6. Delete a task`
+//     )
+//   );
 
-  switch (choice) {
-    case 1:
-      switchList();
-      break;
-    case 2:
-      addTask();
-      break;
-    case 3:
-      newList();
-      break;
-    case 4:
-      editTask();
-      break;
-    case 5:
-      viewListTasks();
-      break;
-    case 6:
-      deleteTask();
-      break;
-  }
-}
+//   switch (choice) {
+//     case 1:
+//       switchList();
+//       break;
+//     case 2:
+//       addTask();
+//       break;
+//     case 3:
+//       newList();
+//       break;
+//     case 4:
+//       editTask();
+//       break;
+//     case 5:
+//       viewListTasks();
+//       break;
+//     case 6:
+//       deleteTask();
+//       break;
+//   }
+// }
