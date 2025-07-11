bd3990a5e0bcf81bb15162afa59ec41b80f0e30a
“Nik <nikolaspreyes@gmail.com>
2025-07-11 12:23:23 -0700

    refactor: create nav, add task button, add text to specify type of button in createAddButton

--- src/components/AddingButton/CreateAddButton.js
+++ src/components/AddingButton/CreateAddButton.js
@@ -2,13 +2,14 @@ import generateElement from "../../utils/GenerateElement.js";
 import "./addButton.css";
 
 export default function createAddButton(attributes) {
+  console.log(attributes);
   const addListButtonWrapper = generateElement("div", {
     class: `${attributes.class}-wrapper add-btn-wrapper`,
   });
   const addListButton = generateElement(
     "button",
     { class: `${attributes.class}-btn add-btn` },
-    "+"
+    attributes.text || "+"
   );
 
   addListButtonWrapper.appendChild(addListButton);
--- src/components/Header/CreateHeader.js
+++ src/components/Header/CreateHeader.js
@@ -1,13 +1,18 @@
-import generateElement from "../../utils/GenerateElement";
-import createHamburger from "../Hamburger/CreateHamburger";
+import generateElement from "../../utils/GenerateElement.js";
+import createHamburger from "../Hamburger/CreateHamburger.js";
+import createAddButton from "../AddingButton/CreateAddButton.js";
 import "./header.css";
 
 export default function createHeader(listName) {
   const header = generateElement("header", { class: "header" });
   const hamburger = createHamburger();
+  const addTaskBtn = createAddButton({ class: "add-task", text: "Add Task" });
+  const nav = generateElement("nav", { class: "navigation toolbar" });
+  nav.appendChild(addTaskBtn);
   const titleWrapper = generateElement("div", {
     class: "header-title-wrapper",
   });
+
   const headerTitle = generateElement(
     "h2",
     { class: "header-title" },
@@ -15,7 +20,7 @@ export default function createHeader(listName) {
   );
 
   titleWrapper.appendChild(headerTitle);
-  header.append(titleWrapper, hamburger);
+  header.append(titleWrapper, hamburger, nav);
 
   return header;
 }
