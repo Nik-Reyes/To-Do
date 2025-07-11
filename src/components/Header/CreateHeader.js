1b94030372416b47d02cb035b903092822d04d04
“Nik <nikolaspreyes@gmail.com>
2025-07-11 00:13:11 -0700

    style: redesign header

--- src/components/Header/CreateHeader.js
+++ src/components/Header/CreateHeader.js
@@ -5,13 +5,17 @@ import "./header.css";
 export default function createHeader(listName) {
   const header = generateElement("header", { class: "header" });
   const hamburger = createHamburger();
+  const titleWrapper = generateElement("div", {
+    class: "header-title-wrapper",
+  });
   const headerTitle = generateElement(
     "h2",
     { class: "header-title" },
     listName.toUpperCase()
   );
 
-  header.append(headerTitle, hamburger);
+  titleWrapper.appendChild(headerTitle);
+  header.append(titleWrapper, hamburger);
 
   return header;
 }
