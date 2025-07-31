eda715629c17c611f03f1cc6bf1f3401b46f3dfe
“Nik <nikolaspreyes@gmail.com>
2025-07-30 17:36:12 -0700

    refactor: remove console logs

--- src/components/TaskCollections/AllTasks/AllTasksCollection.js
+++ src/components/TaskCollections/AllTasks/AllTasksCollection.js
@@ -2,7 +2,6 @@ import generateElement from "../../../utils/GenerateElement.js";
 import "./allTasks.css";
 
 export default function createAllTasksTaskCollection(collectionState) {
-  console.log(collectionState);
   return generateElement("section", {
     class: "task-collection all-tasks",
     "data-collection-state": collectionState,
--- src/components/TaskCollections/Generic/GenericTaskCollection.js
+++ src/components/TaskCollections/Generic/GenericTaskCollection.js
@@ -2,7 +2,6 @@ import generateElement from "../../../utils/GenerateElement.js";
 import "./genericCollection.css";
 
 export default function createGenericTaskCollection(collectionState) {
-  console.log(collectionState);
   return generateElement("section", {
     class: "task-collection generic",
     "data-collection-state": collectionState,
